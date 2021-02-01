4a8643d461d633ba1fc8aa579a4fc10669b4fc5a
Anton Afanasiev <ant.afanasev@gmail.com>
2021-02-01 18:02:03 +0300

    corrected route

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -7,7 +7,7 @@ cardsRouter.get('/cards', cardsList);
 cardsRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().custom().pattern(/^(https?:\/\/)(www\.)?([\w\W\d]+)(\.)([a-z]{1,10})([\w\W\d]+)?$/),
+    link: Joi.string().required().pattern(/^(https?:\/\/)(www\.)?([\w\W\d]+)(\.)([a-z]{1,10})([\w\W\d]+)?$/),
   }),
 }), createCard);
 
