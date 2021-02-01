d0c685d1891bbe38212d5516df03895d98a5fa0a
Anton Afanasiev <ant.afanasev@gmail.com>
2021-02-01 23:30:40 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,14 @@ const NotFoundErr = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use(cors());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
