eadeb52687319c2ae6edc4dd09abb92623cef51c
Anton Afanasiev <ant.afanasev@gmail.com>
2021-02-04 19:51:49 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(cors({
-  origin: 'https://api.mestoproject.students.nomoredomains.work',
+  origin: 'https://mestoproject.students.nomoredomains.work',
   credentials: true,
 }));
 
