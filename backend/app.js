dcc4d2c031f4b03a2db22be51800be53566f12e5
Anton Afanasiev <ant.afanasev@gmail.com>
2021-02-04 23:27:06 +0300

    auth fix

--- backend/app.js
+++ backend/app.js
@@ -62,11 +62,13 @@ app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
-  if (err.status) {
-    res.status(err.status).send(err.message);
-    return;
-  }
-  res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'Ошибка на стороне сервера' : message,
+    });
   next();
 });
 
